edb8fb5628d81ebbac7a83ef327e5702236ba5ab
Mario Rodriguez <mariorodbel@gmail.com>
2022-03-03 19:02:02 +0100

    Final

--- tests/test_api.py
+++ tests/test_api.py
@@ -6,6 +6,8 @@ DUMMY_ID = 0
 BASE_URL = "/api"
 ARTISTS_URL = "/artists/"
 ALBUMS_URL = "/albums/"
+TRACKS_URL = "/tracks"
+GENRES_URL = "/genres"
 
 
 class TestApi:
@@ -14,6 +16,10 @@ class TestApi:
     artist_1_id = artist_1_etag = artist_1 = None
     album_1_id = album_1_etag = album_1 = None
     album_2_id = album_2_etag = album_2 = None
+    track_1_id = track_1_etag = track_1 = None
+    track_2_id = track_2_etag = track_2 = None
+    genre_1_id = genre_1_etag = genre_1 = None
+    genre_2_id = genre_2_etag = genre_2 = None
 
     def test_artists_url(self, test_client):
         """GET ARTISTS"""
@@ -205,10 +211,168 @@ class TestApi:
         )
         assert ret.status_code == 204
 
-        # GET list
+    def test_tracks_url(self, test_client):
+        """GET TRACKS"""
+        ret = test_client.get(BASE_URL + TRACKS_URL)
+        assert ret.status_code == 200
+        assert ret.json == []
+
+    def test_tracks_post(self, test_client):
+        """ADD TRACKS"""
+        TestApi.track_1 = {"name": "Go Down"}
+
+        ret = test_client.post(BASE_URL + TRACKS_URL, json=TestApi.track_1)
+        assert ret.status_code == 201
+
+        ret_val = ret.json
+        TestApi.track_1_id = ret_val.pop("id")
+        TestApi.track_1_etag = ret.headers["ETag"]
+        assert ret_val == TestApi.track_1
+
+    def test_tracks_get_list(self, test_client):
+        """GET TRACKS LIST"""
+        ret = test_client.get(BASE_URL + TRACKS_URL)
+        assert ret.status_code == 200
+
+        ret_val = ret.json
+        assert len(ret_val) == 1
+        assert ret_val[0]["id"] == TestApi.track_1_id
+
+    def test_tracks_get_by_id(self, test_client):
+        """GET TRACKS BY ID"""
+        ret = test_client.get(BASE_URL + TRACKS_URL + str(TestApi.track_1_id))
+        assert ret.status_code == 200
+        assert ret.headers["ETag"] == TestApi.track_1_etag
+
+        ret_val = ret.json
+        ret_val.pop("id")
+        assert ret_val == TestApi.track_1
+
+    def test_tracks_put(self, test_client):
+        """PUT TRACKS"""
+        TestApi.track_1.update({"name": "Go Down"})
+
+        ret = test_client.put(
+            BASE_URL + TRACKS_URL + str(TestApi.track_1_id),
+            json=TestApi.track_1,
+            headers={"If-Match": TestApi.track_1_etag},
+        )
+        assert ret.status_code == 200
+
+        ret_val = ret.json
+        ret_val.pop("id")
+        TestApi.track_1_etag = ret.headers["ETag"]
+        assert ret_val == TestApi.track_1
+
+    def test_tracks_put404(self, test_client):
+        """PUT TRACKS WITH WRONG ID"""
+        ret = test_client.put(
+            BASE_URL + TRACKS_URL + str(DUMMY_ID),
+            json=TestApi.track_1,
+            headers={"If-Match": TestApi.track_1_etag},
+        )
+        assert ret.status_code == 404
+
+    def test_genres_url(self, test_client):
+        """GET GENRES"""
+        ret = test_client.get(BASE_URL + GENRES_URL)
+        assert ret.status_code == 200
+        assert ret.json == []
+
+    def test_genres_post(self, test_client):
+        """ADD GENRES"""
+        TestApi.genre_1 = {"name": "Rock"}
+
+        ret = test_client.post(BASE_URL + GENRES_URL, json=TestApi.genre_1)
+        assert ret.status_code == 201
+
+        ret_val = ret.json
+        TestApi.genre_1_id = ret_val.pop("id")
+        TestApi.genre_1_etag = ret.headers["ETag"]
+        assert ret_val == TestApi.genre_1
+
+    def test_genre_get_list(self, test_client):
+        """GET GENRES LIST"""
+        ret = test_client.get(BASE_URL + GENRES_URL)
+        assert ret.status_code == 200
+
+        ret_val = ret.json
+        assert len(ret_val) == 1
+        assert ret_val[0]["id"] == TestApi.genre_1_id
+
+    def test_genres_get_by_id(self, test_client):
+        """GET GENRES BY ID"""
+        ret = test_client.get(BASE_URL + GENRES_URL + str(TestApi.genre_1_id))
+        assert ret.status_code == 200
+        assert ret.headers["ETag"] == TestApi.genre_1_etag
+
+        ret_val = ret.json
+        ret_val.pop("id")
+        assert ret_val == TestApi.genre_1
+
+    def test_genre_put(self, test_client):
+        """PUT GENRES"""
+        TestApi.genre_1.update({"name": "Rock"})
+
+        ret = test_client.put(
+            BASE_URL + GENRES_URL + str(TestApi.genre_1_id),
+            json=TestApi.genre_1,
+            headers={"If-Match": TestApi.genre_1_etag},
+        )
+        assert ret.status_code == 200
+
+        ret_val = ret.json
+        ret_val.pop("id")
+        TestApi.genre_1_etag = ret.headers["ETag"]
+        assert ret_val == TestApi.genre_1
+
+    def test_genres_put404(self, test_client):
+        """PUT GENRES WITH WRONG ID"""
+        ret = test_client.put(
+            BASE_URL + GENRES_URL + str(DUMMY_ID),
+            json=TestApi.genre_1,
+            headers={"If-Match": TestApi.genre_1_etag},
+        )
+        assert ret.status_code == 404
+
+    def test_tracks_delete(self, test_client):
+        """DELETE TRACKS"""
+        ret = test_client.delete(
+            BASE_URL + TRACKS_URL + str(TestApi.track_2_id),
+            headers={"If-Match": TestApi.track_2_etag},
+        )
+        assert ret.status_code == 204
+
+    def test_genres_delete(self, test_client):
+        """DELETE GENRES"""
+        ret = test_client.delete(
+            BASE_URL + GENRES_URL + str(TestApi.genre_2_id),
+            headers={"If-Match": TestApi.genre_2_etag},
+        )
+        assert ret.status_code == 204
+
+        # GET list tracks
+        ret = test_client.get(BASE_URL + TRACKS_URL)
+        assert ret.status_code == 200
+        assert ret.json == []
+
+        # GET list genres
+        ret = test_client.get(BASE_URL + GENRES_URL)
+        assert ret.status_code == 200
+        assert ret.json == []
+
+        # GET list artists
         ret = test_client.get(BASE_URL + ARTISTS_URL)
         assert ret.status_code == 200
         assert ret.json == []
+
+        # GET by id tracks -> 404
+        ret = test_client.get(BASE_URL + TRACKS_URL + str(TestApi.track_1_id))
+        assert ret.status_code == 404
+
+        # GET by id genres -> 404
+        ret = test_client.get(BASE_URL + GENRES_URL + str(TestApi.genre_1_id))
+        assert ret.status_code == 404
 
         # GET by id artists -> 404
         ret = test_client.get(BASE_URL + ARTISTS_URL + str(TestApi.artist_1_id))
